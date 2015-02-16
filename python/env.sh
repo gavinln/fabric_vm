68b08f8b194fb1fffb5e0bea4db2d628f7127b66
Gavin <gavinln@hotmail.com>
2015-02-16 23:52:19 +0000

    Added script to set aliases

---
+++ python/env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Usage: source env.sh
+
+DEPLOY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+function system-info() {
+  fab -f $DEPLOY_DIR/fabfile.py $*
+}
+
+alias si='system-info'
+alias si-host-type='system-info host-type'
+alias si-ssh-config='system-info ssh-config'
+
